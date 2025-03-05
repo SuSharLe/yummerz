2789522650c7d5db989473df9bc6e0cdac237f13
Su_Shar_Le <sushareducation15@gmail.com>
2025-03-05 22:28:37 +0630

    first commit

---
+++ admin/chart1.js
@@ -0,0 +1,28 @@
+const ctx = document.getElementById("barchart");
+
+new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: [
+      "Mac and Cheese",
+      "Fried Ice Cream",
+      "Burger",
+      "Pizza",
+      "Fish and Chips",
+    ],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12000, 19000, 6000, 9000, 3000, 3000],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
